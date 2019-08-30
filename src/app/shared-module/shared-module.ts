335c6167f711855d19b8d15273602d4482e9e587
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-08-30 11:52:09 +0200

    Fixes shared module name

--- src/app/shared-module/shared-module.ts
+++ src/app/shared-module/shared-module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    TranslateModule
+    TranslateModule,
+    HeaderComponent
   ]
 })
-export class SharedModuleModule { }
+export class SharedModule { }
