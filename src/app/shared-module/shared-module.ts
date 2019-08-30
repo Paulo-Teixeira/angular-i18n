0b77ab6aacbc327649715298c6f6269fdfdf2349
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-08-30 14:53:20 +0200

    Removes translate module from shared module

--- src/app/shared-module/shared-module.ts
+++ src/app/shared-module/shared-module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './header/header.component';
-
 
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    TranslateModule,
-    HeaderComponent
-  ]
+  imports: [CommonModule],
+  exports: [HeaderComponent]
 })
 export class SharedModule { }
