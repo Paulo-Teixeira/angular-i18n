892d9b570a21d529b99d875b2251ed9cfca341f7
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-09-02 11:50:49 +0200

    Updates shared module with toolbar component

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
+  declarations: [
+    ToolbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
+  ],
+  exports: [
+    ToolbarComponent
   ]
 })
 export class SharedModule { }
