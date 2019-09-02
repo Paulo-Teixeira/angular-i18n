c0204aa910fd40516551395eb6b91f96eb6dc6a6
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-09-02 14:14:58 +0200

    Moves language selector from app module to shared toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -9,12 +8,6 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr', 'pt']);
-    translate.setDefaultLang('en');
-
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr|pt/) ? browserLang : 'en');
-  }
+  constructor() { }
   
 }
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'fr', 'pt']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr|pt/) ? browserLang : 'en');
+  }
 
   ngOnInit() {
   }
